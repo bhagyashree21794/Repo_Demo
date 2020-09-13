4f18627228fe6d346432ee69df388437cce4152e
GHK <GHK@DESKTOP-CUMKH4R>
2020-09-13 13:11:11 +0530

    First prog to launch browser

---
+++ src/org/qsp/weekend/ioDemo/DemoLaunch.java
@@ -0,0 +1,11 @@
+package org.qsp.weekend.ioDemo;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoLaunch {
+	public static void main(String[] args){
+		System.setProperty("webdriver.chrome.driver", ".\\Software\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+	}
+
+}
